85cf6d1d83d465bb7b7908527e07bfe208400fdf
YannisGionis <gionjo@gmnail.com>
2025-10-03 16:29:27 +0300

    plugin.php debug

--- wp-blocks-gutenberg-mega-menu/plugin.php
+++ wp-blocks-gutenberg-mega-menu/plugin.php
@@ -1,27 +1,5 @@
 <?php
 /**
- * Plugin Name:       Team Members
- * Description:       A team members grid.
- * Requires at least: 5.7
- * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            Ali Alaa
- * License:           GPL-2.0-or-later
- * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain:       team-members
- *
- * @package           blocks-course
- */
-
-/**
- * Registers the block using the metadata loaded from the `block.json` file.
- * Behind the scenes, it registers also all assets so they can be enqueued
- * through the block editor in the corresponding context.
- *
- * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
- */
-<?php
-/**
  * Plugin Name:       Gutenberg Mega Menu
  * Description:       Gutenberg Mega Menu (desktop only).
  * Requires at least: 5.7
